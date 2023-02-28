58a2635e101240838536bf6e23de2639ea09024b
Ericka Vea <erickavea3525@gmail.com>
2023-02-28 11:19:54 -0800

    botonBasico

--- src/views/SecondView.vue
+++ src/views/SecondView.vue
@@ -1,7 +1,13 @@
 <template>
     <table-main />
+    <MiBotonBasico :etiqueta = "etiqueta"></MiBotonBasico>
   </template>
   
   <script setup>
+    import { ref } from 'vue';
     import TableMain from '@/components/TablaMain.vue'
+    import MiBotonBasico from '@/components/MiBotonBasico.vue'
+    
+
+    const etiqueta = ref("OKAY");
   </script>
